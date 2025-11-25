89a8e065c4099902e8aaecff0be87651deb1d930
Aditya Rao <adityarao2005@gmail.com>
2025-11-24 23:47:08 -0500

    updated the python project to use docker to build the resume

---
+++ Dockerfile
@@ -0,0 +1,18 @@
+FROM pandoc/latex
+
+# Install Python3 and pip
+RUN apk add --no-cache python3 py3-pip
+
+WORKDIR /app
+
+COPY pyproject.toml .
+
+RUN python3 -m venv venv
+ENV PATH="/app/venv/bin:$PATH"
+RUN pip3 install --no-cache-dir pdm-backend
+RUN pip3 install --no-cache-dir .
+
+COPY src/ ./src/
+COPY templates/ ./templates/
+
+ENTRYPOINT ["python3", "-m", "src.resume_ci_automation"]
---
+++ src/resume_ci_automation/__main__.py
@@ -0,0 +1,21 @@
+import subprocess
+import yaml
+from jinja2 import Environment, FileSystemLoader
+
+# Load YAML data
+with open("data/resume.yaml", "r") as file:
+    data = yaml.safe_load(file)
+
+# Render template
+env = Environment(loader=FileSystemLoader("templates"))
+template = env.get_template("resume_template.tex.j2")
+output = template.render(**data)
+
+if __name__ == "__main__":
+
+    with open("resume.tex", "w") as f:
+        f.write(output)
+
+    subprocess.run(["pdflatex", "resume.tex", "-o", "out/resume.pdf"])
+
+    print("Resume CI Automation Package")
